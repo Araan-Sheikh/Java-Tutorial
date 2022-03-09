a87450ee98705b45a73cfb9296a194a14bdae52c
Araan Sheikh <90261325+Araan-Sheikh@users.noreply.github.com>
2022-03-09 21:15:09 +0530

    Create division.java

---
+++ division of number/division.java
@@ -0,0 +1,11 @@
+class division
+{  
+ public static void main(String args[])   
+ {  
+ int n1 , n2 , division;
+ n1 = 20;
+ n2 = 10;
+ division = n1/n2;  
+ System.out.println("The division of numbers is: "+division);  
+ }  
+} 
