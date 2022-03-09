d44ba9a31b8690ca58cc822d602e654475c639de
Araan Sheikh <90261325+Araan-Sheikh@users.noreply.github.com>
2022-03-09 21:12:29 +0530

    Create sum.java

---
+++ sum of number/sum.java
@@ -0,0 +1,11 @@
+class sum
+{  
+ public static void main(String args[])   
+ {  
+ int n1 , n2 , sum;
+ n1 = 20;
+ n2 = 10;
+ sum = n1 + n2;  
+ System.out.println("The sum of numbers is: "+sum);  
+ }  
+} 
