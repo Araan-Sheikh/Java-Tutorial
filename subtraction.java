59c0669949faef3c115de0fdd5f9478f8535637e
Araan Sheikh <90261325+Araan-Sheikh@users.noreply.github.com>
2022-03-10 07:32:56 +0530

    Create subtraction.java

---
+++ subtraction.java
@@ -0,0 +1,11 @@
+class subtraction 
+{  
+ public static void main(String args[])   
+ {  
+ int n1 , n2 , subtraction;
+ n1 = 20;
+ n2 = 10;
+ subtraction = n1 + n2;  
+ System.out.println("The subtraction of numbers is: "+subtraction);  
+ }  
+} 
